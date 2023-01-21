fc105c7f0e0d496c2fb1b439c3a3454cce9ae2b5
vboucher07 <97803777+vboucher07@users.noreply.github.com>
2023-01-21 13:07:06 -0500

    working sensors

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
+#include <HCSR04.h>
+#include <TimerOne.h>
 
 #define COLOR_SENSOR1_ANALOG_PIN A1
 #define COLOR_SENSOR1_DIGITAL_PIN 3
 #define COLOR_SENSOR2_ANALOG_PIN A2
 #define COLOR_SENSOR2_DIGITAL_PIN 5
-#define DISTANCE_TRIG_PIN 10
-#define DISTANCE_ECHO_PIN 9
+#define DISTANCE_TRIG_PIN 6
+#define DISTANCE_ECHO_PIN 5
 #define LINE_THRESHOLD 60
 int Line1_Color = 0;
 int Line2_Color = 0;
@@ -13,10 +15,17 @@ boolean Line1_OnLine = false;
 boolean Line2_OnLine = false;
 boolean onLine = false;
 float distance = 0;
-long duration = 0;
+volatile int timerFlag = 0;
+UltraSonicDistanceSensor distanceSensor(DISTANCE_TRIG_PIN, DISTANCE_ECHO_PIN);
+
+boolean isOnLine();
+float getDistance();
+void timerISR();
 
 void setup() {
   Serial.begin(9600);
+  Timer1.initialize(50000);
+  Timer1.attachInterrupt(timerISR);
 
   pinMode(COLOR_SENSOR1_ANALOG_PIN, INPUT);
   pinMode(COLOR_SENSOR1_DIGITAL_PIN, INPUT);
@@ -28,6 +37,15 @@ void setup() {
 
 void loop() {
   isOnLine();
+  if(timerFlag){
+    distance = distanceSensor.measureDistanceCm();
+    timerFlag = 0;
+  }
+  Serial.print("OnLine: ");
+  Serial.print(Line1_OnLine);
+  Serial.print(" || Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
 }
 
 boolean isOnLine(){
@@ -38,10 +56,6 @@ boolean isOnLine(){
   return false; //TODO
 }
 
-float getDistance(){
-  digitalWrite(DISTANCE_ECHO_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(DISTANCE_ECHO_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(DISTANCE_ECHO_PIN, LOW);
+void timerISR(){
+  timerFlag = 1;
 }
